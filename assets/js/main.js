b26131d9f93939703cf829df17fb59cf9c3c2033
samiradayib <samiradayib1@gmail.com>
2025-02-16 21:39:21 -0500

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+AOS.init({
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, 
+  duration: 900, 
+  easing: 'ease', // default easing for AOS animations
+  once: false, 
+  mirror: false, 
+  anchorPlacement: 'top-bottom', 
+
+});
